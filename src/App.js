215e800f8a39350d949c922fc08c4a17e1cd67df
rafeym <mohammadrafey0@gmail.com>
2021-03-16 17:20:45 -0400

    DEVELOP: Addition of react router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Header from './components/Header'
+import Section from './components/Section'
 
 const App = () => {
-  return <div className='app'>Home Page</div>
+  return (
+    <div className='app'>
+      <Router>
+        <Header />
+        <Section />
+      </Router>
+    </div>
+  )
 }
 
 export default App
